f9347e347c3cab7e5ca96c8c8ad0d2feccc6eb41
Erik Olsen <eolsen@luxoft.com>
2017-06-28 14:00:08 -0700

    MSRETEL-380 msft retail campaign data feeds

---
+++ ResApplicationTelemetry/dbo/Views/vwMrcHtcOculusWaiver.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[vwMrcHtcOculusWaiver] AS
+SELECT
+    tbl1.id as 'ID',
+    tbl1.CompletedDate, 
+	tbl1.StoreNumber,
+	tbl1.EnvelopeID, 
+	tbl1.Experience
+FROM 
+    ViveOculusExperienceReport tbl1
