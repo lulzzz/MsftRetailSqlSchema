9acc36bfa73ed18b98852425ca05ba08d2d2e77b
Erik Olsen <eolsen@luxoft.com>
2017-06-30 08:32:19 -0700

    MSRETEL-380 msft retail campaign data feeds

---
+++ ResApplicationTelemetry/dbo/Views/vwMrcArvrDocuSignWaiver.sql
@@ -0,0 +1,12 @@
+﻿-- MSRETEL-380 (06.30.2017): microsoft retail campaign data feed
+
+CREATE VIEW [dbo].[vwMrcArvrDocuSignWaiver] AS
+SELECT
+    tbl1.Id as 'ID',
+    tbl1.CompletedDate as 'CompletedDateTime',
+	CONVERT(date,  tbl1.CompletedDate) as 'CompletedDate',
+	tbl1.StoreNumber,
+	tbl1.EnvelopeID, 
+	tbl1.Experience
+FROM 
+    ViveOculusExperienceReport tbl1
--- ResApplicationTelemetry/dbo/Views/vwMrcHtcOculusWaiver.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW [dbo].[vwMrcHtcOculusWaiver] AS
-SELECT
-    tbl1.id as 'ID',
-    tbl1.CompletedDate, 
-	tbl1.StoreNumber,
-	tbl1.EnvelopeID, 
-	tbl1.Experience
-FROM 
-    ViveOculusExperienceReport tbl1
