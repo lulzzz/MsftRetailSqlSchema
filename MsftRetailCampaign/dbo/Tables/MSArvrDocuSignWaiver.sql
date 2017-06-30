873079cbd97405c3ce3f85f685474f1f1c27d1b6
Erik Olsen <eolsen@luxoft.com>
2017-06-30 09:20:08 -0700

    MSRETEL-380 msft retail campaign data feeds

---
+++ MsftRetailCampaign/dbo/Tables/MSArvrDocuSignWaiver.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MSArvrDocuSignWaiver] (
+    [ID]                BIGINT           IDENTITY (1, 1) NOT NULL,
+    [DateLoaded]        DATETIME         DEFAULT (getdate()) NULL,
+    [CompletedDateTime] DATETIME         NULL,
+    [CompletedDate]     DATE             NULL,
+    [StoreNumber]       INT              NULL,
+    [EnvelopeID]        UNIQUEIDENTIFIER NULL,
+    [Experience]        VARCHAR (100)    NULL,
+    CONSTRAINT [PK_MSArvrDocuSignWaiver_ID] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
